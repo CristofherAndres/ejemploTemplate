f7942fde5517ab07259e213a6ecd8a96925e79ff
Cristofher <Cristofherrojas.cr@gmail.com>
2023-09-06 10:04:46 -0300

    first commit

---
+++ primeraApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def llamarTemplate(request):
+    return render(request, 'primeraApp/primerTemplate.html')
+
+
+def infoUsuario(request):
+    data = {
+        'nombre': 'Pedro',
+        'apellido': 'Parker',
+        'correo': 'peterparker@dayli.cl',
+        'direccion': 'New York, Casa tia May'
+    }
+    return render(request, 'primeraApp/segundoTemplate.html',data)
